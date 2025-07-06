726dbe45d6f08f1705383c617329a9c7211e3c37
Krish Maiti <krishnakantamaiti7337@gmail.com>
2025-07-06 11:59:53 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,8 @@ from dataclasses import dataclass # used to create class variable
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 
@@ -70,8 +72,12 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    train_data , test_data = obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-    # call the function of data transformation
+    # Data Transformation
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    # Model Training
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
